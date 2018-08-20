69c58159953dcb20cdaccd6238351df804dbd77b
Patryk Smith <32315397+PatrykJS@users.noreply.github.com>
2018-08-20 17:48:42 -0400

    Made it look pretty

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main
   	System.out.println("\n\n\n");
   	System.out.println(" ________________________________________________________________________");
   	System.out.println("|\t\t\t\t\t\t\t\t\t|");
-    System.out.print("|\tA new output line can also be produced by inserting \\n,\t\t|\n");
+        System.out.print("|\tA new output line can also be produced by inserting \\n,\t\t|\n");
 	System.out.print("|\tknown as a newline character, within a string literal. \t\t|\n");
 	System.out.println("|\tEx: Outputting \"1\\n2\\n3\" outputs each number on its own \t|");
 	System.out.println("|\toutput line. \\n consists of two characters, \\ and n, but \t|");
