28ca26862288c55fda27cf66c667aabbfc76392c
Patryk Smith <32315397+rgbq@users.noreply.github.com>
2018-08-20 17:24:25 -0400

    Patryk Smith - Hello World Project - repl.it

---
+++ Main.java
@@ -0,0 +1,26 @@
+// John Doe
+
+public class Main 
+{
+  
+  public static void main(String[] args) 
+  {
+  	
+  	System.out.println("\n\n\n");
+  	System.out.println(" ________________________________________________________________________");
+  	System.out.println("|\t\t\t\t\t\t\t\t\t|");
+    System.out.print("|\tA new output line can also be produced by inserting \\n,\t\t|\n");
+	System.out.print("|\tknown as a newline character, within a string literal. \t\t|\n");
+	System.out.println("|\tEx: Outputting \"1\\n2\\n3\" outputs each number on its own \t|");
+	System.out.println("|\toutput line. \\n consists of two characters, \\ and n, but \t|");
+	System.out.println("|\ttogether are considered as one newline character. Good \t\t|");
+	System.out.println("|\tractice is to use println to output a newline when possible, \t|");
+	System.out.println("|\tas println has some technical advantages not mentioned here.\t|");
+	System.out.println("|_______________________________________________________________________|");
+	System.out.println("\n\n");
+	
+  }
+  
+}
+
+
